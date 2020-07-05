e17958d5481a29ba2a2af72ad93da8f63b78a24c
Ahmed Shalaby <kingshalaby@yahoo.com>
2020-07-05 23:43:34 +0200

    Initial Commit

---
+++ queue_list/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  queue_list
+//
+//  Created by ahmed shalaby on 7/5/20.
+//  Copyright © 2020 ahmed shalaby. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
